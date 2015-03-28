f9d8c0e43cf2e331781f4aedbd8bdd174b98eebb
fe-rod <federicorodriguezabo@gmail.com>
2015-03-28 02:33:57 -0300

    stores form design

--- website/wizard/app/controllers/stores.js
+++ website/wizard/app/controllers/stores.js
@@ -3,12 +3,53 @@
 
     angular
         .module('menudeldia')
-        .controller('storeCtrl', stores);
+        .controller('storesCtrl', stores);
 
-    stores.$inject = ['$scope'];
+    stores.$inject = ['$scope', '$timeout'];
 
-    function stores($scope) {
+    function stores($scope, $timeout) {
 
+        $scope.stores = [];
+
+        $scope.showForm = false;
+
+        if($scope.stores.length == 0)
+            $scope.showForm = true;
+
+        $scope.addStore = function(){
+            $scope.showForm = true;
+        }
+
+        $scope.days = [
+            { name: 'Lunes', from: '', to:'', open: true},
+            { name: 'Martes', from: '', to:'', open: true},
+            { name: 'Miércoles', from: '', to:'', open: true},
+            { name: 'Jueves', from: '', to:'', open: true},
+            { name: 'Viernes', from: '', to:'', open: true},
+            { name: 'Sábado', from: '', to:'', open: true},
+            { name: 'Domingo', from: '', to:'', open: true}
+        ];
+
+        $scope.loadingSave = false;
+        $scope.loadingNextStep = false;
+
+        $scope.save = function(){
+            $scope.loadingSave = true;
+            $timeout(function(){
+                $scope.loadingSave = false;
+            }, 3000)
+        }
+
+        $scope.nextStep = function(){
+            $scope.loadingNextStep = true;
+            $timeout(function(){
+                $scope.loadingNextStep = false;
+            }, 3000)
+        }
+
+        $scope.toggleOpenDay = function(day){
+            day.open = !day.open;
+        }
     }
 })();
 
